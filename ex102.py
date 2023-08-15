3dfdba833453403f84af7cf6e4245ccfc9f120e0
LTWzin <witalogabrielcruznascimento@gmail.com>
2023-08-15 14:30:08 -0300

    15/08/2023

---
+++ ex102.py
@@ -0,0 +1,20 @@
+def fatorial(num=1, show=False):
+    a = 1
+    if show == False:  # Sem Mostrar a multiplicação
+        while num != 1:
+            a *= num
+            num -= 1
+        return a
+    else:  # Mostrando a multiplicação
+        while num != 1:
+            print(f'{num} x ', end= '')
+            a *= num
+            num -= 1
+        print('1 = ', end= '')
+        return a
+
+
+print('-' * 38)
+numero = int(input('Digite um numero para fatorar:  '))
+print('-=' * 19)
+print(fatorial(numero, show=True))
